4b6df2201d26685017940e9668cdda1373f8065c
hyunwoomemo <archihw94@gmail.com>
2022-05-17 14:41:53 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// 상단 메뉴 고정
+var $header = $('header');
+ $(window).scroll(function(){
+     if($(this).scrollTop() > 0){
+        $header.addClass('sticky');
+     }else{
+        $header.removeClass('sticky');
+     }
+ });
+//로딩바
+ $(window).load(function(){
+    $('.loader').delay('3000').fadeOut();
+});
